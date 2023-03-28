f7bdcfc24027674bf2ad869ca987c4862c22b858
Katy Kam <katyky14@gmail.com>
2023-03-27 21:24:15 -0700

    actual code

--- problems.js
+++ problems.js
@@ -76,12 +76,40 @@ var search = function(nums, target) {
         // reassing left mid minus one
 
 
+    let left =  0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = (right + left ) / 2; //average for mid point
+        if (target === nums[mid]) {
+            return mid;
+        }
+
+        // left sorted portion
+        if (nums[left] <= nums[mid]) {
+            if (target > nums[mid] || target < nums[left]) {
+                left = mid + 1
+            } else {
+                right = mid - 1;
+            }
+        } else {
+            if (target < nums[mid] || target > nums[right]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1
+            }
+        }
+
+    }
+
+    return -1;
   };
 
 
 
-
-
+let nums = [4,5,6,7,0,1,2];
+let target = 0
+console.log(search(nums, target)) // 4
 
 
 /************************************************************************* */
