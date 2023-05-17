36029385887ed7e0b137bccbca6f1c5337fc6a36
Katy Kam <katyky14@gmail.com>
2023-05-17 15:31:16 -0700

    double letter count

--- problems.js
+++ problems.js
@@ -1,7 +1,30 @@
 
 
 
+/*
+Double Letter Count
+Write a function doubleLetterCount that takes in a string and returns the
+ number of times that the same letter repeats twice in a row.
 
+
+*/
+
+function doubleLetterCount(string) {
+    // your code here
+  let count = 0;
+
+  for (let i = 0; i < string.length; i++) {
+  	if (string[i] === string[i + 1]) {
+        	count++
+        }
+
+  }
+
+  return count
+}
+
+// console.log(doubleLetterCount("the jeep rolled down the hill"));  // 3
+// console.log(doubleLetterCount("bootcamp")); // 1
 
 
 /*
@@ -39,7 +62,28 @@ Constraints:
 */
 
 
+var maxProfit = function(prices) {
+    let maxProfit = 0;
 
+    let left = 0;
+
+    for (let i = 0; i < prices.length ; i++) {
+        let prev = prices[i]
+
+        if (prices[i] > prices[left]) {
+            let profit = prices[i] - prices[left]
+            maxProfit = Math.max(maxProfit, profit)
+        } else {
+            left = i
+        }
+
+    }
+
+
+    return maxProfit
+
+
+};
 
 
 
