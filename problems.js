24425596c48aaaf8cddfe7b36caafc1a417e5730
Katy Kam <katyky14@gmail.com>
2023-05-16 18:44:40 -0700

    best time to buy and sell stock problem

--- problems.js
+++ problems.js
@@ -1,5 +1,50 @@
 
 
+
+
+
+
+/*
+
+BEST TIME TO BUY AND SELL STOCK
+
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+
+
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+
+
+*/
+
+
+
+
+
+
+
+
 /*
 Intersection of Two Arrays II
 
@@ -34,6 +79,21 @@ What if elements of nums2 are stored on disk, and the memory is limited such tha
 */
 
 
+var intersect = function(nums1, nums2) {
+    let res = [];
+
+    for (let i = 0; i < nums1.length; i++) {
+        for (let j = 0; j < nums2.length; j++) {
+            if (nums1[i] === nums2[j]) {
+                res.push(nums1[i]);
+                nums2.splice(j, 1); // Remove the element from nums2 to avoid duplicate counts
+                break; // Exit inner loop since the element has been found
+            }
+        }
+    }
+
+    return res;
+};
 
 
 
