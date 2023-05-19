64153d3c41ac82c03b6e7067e325f08811de92ef
Katy Kam <katyky14@gmail.com>
2023-05-18 19:04:12 -0700

    caesar cipher

--- problems.js
+++ problems.js
@@ -1,5 +1,33 @@
 
 
+
+
+
+
+
+/*
+Caesar Cipher
+Write a method caesarCipher that takes in a string and a number.
+The function should return a new string where every character
+of the original is shifted num characters in the alphabet.
+
+*/
+
+function caesarCipher(string, num) {
+
+
+
+
+}
+
+console.log(caesarCipher("apple", 1)); // "bqqmf"
+console.log(caesarCipher("bootcamp", 2)); // "dqqvecor"
+console.log(caesarCipher("zebra", 4)); // "difve"
+
+
+
+
+
 /*
 
 Vowel Cipher
